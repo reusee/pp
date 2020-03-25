4ad0baed37dbecba9dbc14ee1e3a16e50d711b1e
reus <reusee@gmail.com>
2020-03-25 16:57:08 +0800

    simplify Src.Next

--- pipeline.go
+++ pipeline.go
@@ -4,18 +4,13 @@ type Src func() (any, Src, error)
 
 type Sink func(any) (Sink, error)
 
-func (s *Src) Next() (any, error) {
-	var value any
-	var err error
+func (s *Src) Next() (value any, err error) {
 	for value == nil {
 		if s != nil && *s != nil {
 			value, *s, err = (*s)()
-			if err != nil {
-				return nil, err
-			}
 		} else {
 			break
 		}
 	}
-	return value, nil
+	return
 }
