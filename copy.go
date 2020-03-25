541b283d54238fa6a9473faa692eff8225417bb5
reus <reusee@gmail.com>
2020-03-25 10:03:30 +0800

    allow multiple Sinks in Copy

--- copy.go
+++ copy.go
@@ -1,28 +1,49 @@
 package pp
 
-func Copy(src Src, sink Sink) error {
+func Copy(src Src, sinks ...Sink) error {
 	var err error
 	for {
+
 		var value any
-	step_src:
-		if src != nil {
-			value, src, err = src()
-			if err != nil {
-				return err
-			}
-			if value == nil {
-				goto step_src
-			}
-		}
-		if sink != nil {
-			sink, err = sink(value)
-			if err != nil {
-				return err
+		for value == nil {
+			if src != nil {
+				value, src, err = src()
+				if err != nil {
+					return err
+				}
+			} else {
+				break
 			}
 		}
-		if sink == nil && src == nil {
+
+		if len(sinks) > 0 {
+			for i := 0; i < len(sinks); {
+				sink := sinks[i]
+				if sink == nil {
+					sinks[i] = sinks[len(sinks)-1]
+					sinks = sinks[:len(sinks)-1]
+					continue
+				}
+				sink, err = sink(value)
+				if err != nil {
+					return err
+				}
+				if sink == nil {
+					sinks[i] = sinks[len(sinks)-1]
+					sinks = sinks[:len(sinks)-1]
+					continue
+				}
+				sinks[i] = sink
+				i++
+			}
+		} else {
 			break
 		}
+
+		if len(sinks) == 0 && src == nil {
+			break
+		}
+
 	}
 	return nil
 }
--- copy_test.go
+++ copy_test.go
@@ -31,3 +31,49 @@ func TestCopy(t *testing.T) {
 		t.Fatal()
 	}
 }
+
+func TestCopyToMultipleSinks(t *testing.T) {
+	collect := func(ints *[]int) Sink {
+		var sink Sink
+		sink = func(v any) (Sink, error) {
+			if v == nil {
+				return nil, nil
+			}
+			*ints = append(*ints, v.(int))
+			return sink, nil
+		}
+		return sink
+	}
+
+	emit := func(n int) Src {
+		var src Src
+		src = func() (any, Src, error) {
+			if n == 0 {
+				return nil, nil, nil
+			}
+			n--
+			return n, src, nil
+		}
+		return src
+	}
+
+	var s1, s2, s3 []int
+	if err := Copy(
+		emit(10),
+		collect(&s1),
+		collect(&s2),
+		collect(&s3),
+	); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(s1) != 10 {
+		t.Fatalf("got %d, %v", len(s1), s1)
+	}
+	if len(s2) != 10 {
+		t.Fatalf("got %d, %v", len(s2), s2)
+	}
+	if len(s3) != 10 {
+		t.Fatalf("got %d, %v", len(s3), s3)
+	}
+}
