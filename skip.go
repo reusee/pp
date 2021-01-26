c820d376d0c5d1236837bdd6335d27922a24160a
reus <reusee@gmail.com>
2021-01-26 18:21:33 +0800

    add SkipSrc

---
+++ skip.go
@@ -0,0 +1,20 @@
+package pp
+
+func SkipSrc(src Src, n int, cont Src) Src {
+	var ret Src
+	ret = func() (any, Src, error) {
+		value, err := src.Next()
+		if err != nil {
+			return nil, nil, err
+		}
+		if value == nil && src == nil {
+			return nil, cont, nil
+		}
+		if n > 0 {
+			n--
+			return nil, ret, nil
+		}
+		return value, ret, nil
+	}
+	return ret
+}
---
+++ skip_test.go
@@ -0,0 +1,23 @@
+package pp
+
+import "testing"
+
+func TestSkip(t *testing.T) {
+	var values Values
+	if err := Copy(
+		SkipSrc(
+			Seq(1, 2, 3, 4, 5),
+			1,
+			nil,
+		),
+		CollectValues(&values),
+	); err != nil {
+		t.Fatal(err)
+	}
+	if len(values) != 4 {
+		t.Fatal()
+	}
+	if values[0] != 2 {
+		t.Fatal()
+	}
+}
